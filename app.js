365b3283c2ae083027228d1edee7b66b286c7e28
ZianPayne <zian@payne.name>
2024-05-09 23:28:18 +1200

    Adding basic CRUD

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -20,16 +21,43 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
 app.get('/', (req,res) => {
     res.render('home');
+});
+
+app.get('/campgrounds', async(req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
+});
+
+app.get('/campgrounds/new', async (req,res) => {
+    res.render('campgrounds/new'); 
+});
+
+app.post('/campgrounds', async (req,res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 })
 
-app.get('/makeCampground', async (req,res) => {
-    const camp = new Campground({title : "Mount Vic", description: "Not a campsite"});
-    await camp.save();
-    res.send(camp);
-})
+app.get('/campgrounds/:id', async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground});
+});
 
+app.get('/campgrounds/:id/edit', async(req,res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render(`campgrounds/edit`, {campground});
+});
+
+app.put('/campgrounds/:id', async(req,res) => {
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+});
 
 app.listen(3000, () => {
     console.log("Serving on port 3000")
