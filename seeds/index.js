60a1a34f5490497d420a591329b32c1cea17a34e
ZianPayne <zian@payne.name>
2024-05-09 22:25:32 +1200

    Adding seed code for cities, regions

--- seeds/index.js
+++ seeds/index.js
@@ -1,1 +1,28 @@
+const mongoose = require('mongoose');
+const Campground = require('./../models/campground');
 
+const cities = require('./cities');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection; 
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", () => {
+    console.log("Database connected!!");
+});
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for(let i = 0; i<50;i++){
+        const random20 = Math.floor(Math.random() * 20);
+        const camp = new Campground({
+            location: `${cities[random20].city}, ${cities[random20].region}}`
+        });
+        await camp.save();
+    }
+}
+
+seedDB();
