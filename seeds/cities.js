87248ed23f88cdeafddaaf5e640b4b2a9c4a3552
Z'Arn <119647505+ZianPayne@users.noreply.github.com>
2024-05-09 21:42:29 +1200

    Create cities.js

---
+++ seeds/cities.js
@@ -0,0 +1,155 @@
+module.exports = [
+  {
+    city: "Auckland",
+    growth_from_2013_to_2023: "Unknown", // Data might not be readily available
+    latitude: -36.84846,
+    longitude: 174.76333,
+    population: "1,470,300", // Estimated population for 2023
+    region: "Auckland",
+  },
+  {
+    city: "Christchurch",
+    growth_from_2013_to_2023: "12.5%", // Placeholder, actual data needed
+    latitude: -43.53218,
+    longitude: 172.63623,
+    population: "381,400", // Estimated population for 2023
+    region: "Canterbury",
+  },
+  {
+    city: "Wellington",
+    growth_from_2013_to_2023: "8.7%", // Placeholder, actual data needed
+    latitude: -41.28664,
+    longitude: 174.7786,
+    population: 512,300, // Estimated population for 2023
+    region: "Wellington",
+  },
+  {
+    city: "Dunedin",
+    growth_from_2013_to_2023: "1.3%", // Placeholder, actual data needed
+    latitude: -45.87897,
+    longitude: 170.50861,
+    population: 122,500, // Estimated population for 2023
+    region: "Otago",
+  },
+  {
+    city: "Hamilton",
+    growth_from_2013_to_2023: "15.2%", // Placeholder, actual data needed
+    latitude: -37.79003,
+    longitude: 175.27928,
+    population: 160,400, // Estimated population for 2023
+    region: "Waikato",
+  },
+  {
+    city: "Tauranga",
+    growth_from_2013_to_2023: "22.1%", // Placeholder, actual data needed
+    latitude: -37.7167,
+    longitude: 176.1655,
+    population: 151,500, // Estimated population for 2023
+    region: "Bay of Plenty",
+  },
+  {
+    city: "Napier",
+    growth_from_2013_to_2023: "6.4%", // Placeholder, actual data needed
+    latitude: -39.05611,
+    longitude: 176.87056,
+    population: 66,800, // Estimated population for 2023
+    region: "Hawke's Bay",
+  },
+  {
+    city: "Nelson",
+    growth_from_2013_to_2023: "4.8%", // Placeholder, actual data needed
+    latitude: -41.17499,
+    longitude: 173.27778,
+    population: 48,300, // Estimated population for 2023
+    region: "Nelson Tasman",
+  },
+  {
+    city: "Invercargill",
+    growth_from_2013_to_2023: "-1.7%", // Placeholder, actual data needed
+    latitude: -46.40639,
+    longitude: 168.35085,
+    population: 57,600, // Estimated population for 2023
+    region: "Southland",
+  },
+  {
+    city: "Palmerston North",
+    growth_from_2013_to_2023: "10.1%", // Placeholder, actual data needed
+    latitude: -41.175,
+    longitude: 175.61667,
+    population: 89,300
+  },
+  {
+    city: "Abel Tasman National Park",
+    growth_from_2013_to_2023: "N/A", // Not applicable for parks
+    latitude: -40.90054,
+    longitude: 172.91028,
+    type: "National Park", // Added type for camping location
+  },
+  {
+    city: "Tongariro National Park",
+    growth_from_2013_to_2023: "N/A",
+    latitude: -39.25306,
+    longitude: 175.60000,
+    type: "National Park",
+  },
+  {
+    city: "Lake Tekapo",
+    growth_from_2013_to_2023: "N/A",
+    latitude: -44.31667,
+    longitude: 170.41667,
+    type: "Lake (Camping area)",
+  },
+  {
+    city: "Milford Sound",
+    growth_from_2013_to_2023: "N/A",
+    latitude: -44.40583,
+    longitude: 167.86667,
+    type: "Fiord (Camping area)",
+  },
+  {
+    city: "Franz Josef Glacier",
+    growth_from_2013_to_2023: "N/A",
+    latitude: -43.55000,
+    longitude: 170.11667,
+    type: "Glacier (Camping area)",
+  },
+  {
+    city: "Coromandel Peninsula",
+    growth_from_2013_to_2023: "N/A",
+    latitude: -36.75000,
+    longitude: 175.41667,
+    type: "Peninsula (Camping area)",
+  },
+  {
+    city: "Marlborough Sounds",
+    growth_from_2013_to_2023: "N/A",
+    latitude: -41.33333,
+    longitude: 174.00000,
+    type: "Sounds (Camping area)",
+  },
+  {
+    city: "Ninety Mile Beach",
+    growth_from_2013_to_2023: "N/A",
+    latitude: -34.83333,
+    longitude: 173.41667,
+    type: "Beach (Camping area)",
+  },
+  {
+    city: "Bay of Islands",
+    growth_from_2013_to_2023: "N/A",
+    latitude: -35.25000,
+    longitude: 174.00000,
+    type: "Bay (Camping area)",
+  },
+  {
+    city: "Catlins",
+    growth_from_2013_to_2023: "N/A",
+    latitude: -46.66667,
+    longitude: 170.75000,
+    type: "Coastal area (Camping area)",
+  }
+  ]
+
+
+
+    
