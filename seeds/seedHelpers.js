1a11334b24a5bc76f8a4b5555d7bb87eaf628e87
ZianPayne <zian@payne.name>
2024-05-09 22:03:08 +1200

    commit -m "Helppp"

---
+++ seeds/seedHelpers.js
@@ -0,0 +1,47 @@
+const descriptors = [
+    'lush',
+    'arid',
+    'rocky',
+    'sandy',
+    'volcanic',
+    'snowy',
+    'dense',
+    'sparse',
+    'rugged',
+    'serene',
+    'teeming', // with life
+    'barren',
+    'desolate',
+    'murky', // for water
+    'crystal-clear', // for water
+    'rolling',
+    'flat',
+    'hilly',
+    'misty',
+    'sun-drenched'
+  ];
+  
+  const places = [
+    'jungle',
+    'desert',
+    'canyon',
+    'beach',
+    'riverbed',
+    'glacier',
+    'tundra',
+    'swamp',
+    'cave',
+    'waterfall',
+    'archipelago', // group of islands
+    'fjord',
+    'oasis',
+    'atoll', // ring-shaped coral island
+    'lagoon',
+    'delta',
+    'prairie',
+    'wetland',
+    'volcano',
+    'caldera' // volcanic crater
+  ];
+  
+  module.exports = { descriptors, places };
