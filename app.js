72450150ad915ff3ad05f05c39b19a9ce81b6f45
ZianPayne <zian@payne.name>
2024-06-08 17:33:15 +1200

    Adding Joi

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
 const catchAsync = require('./utils/catchAsync.js');
+const Joi = require('joi');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 
