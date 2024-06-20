6ddf49986960f894208318061fa9ebb2b219cbcd
ZianPayne <zian@payne.name>
2024-06-20 19:46:30 +1200

    Creating Route Logic

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const catchAsync = require('../utils/catchAsync');
+const flash = require('connect-flash');
+
+router.use(flash());
+
+router.get('/register', (req,res) => {
+    res.render('users/register');
+});
+
+router.post('/register', catchAsync(async(req,res) => {
+    try{
+        const {email, username, password} = req.body;
+        const user = new User({email, username, password});
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to YelpCamp!');
+        res.redirect('/campgrounds');
+    } catch(e){
+        console.log(e);
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+}))
+
+module.exports = router;
