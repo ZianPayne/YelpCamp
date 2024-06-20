cad21c53b4184e22747ba339ac07a4dc23257fd8
ZianPayne <zian@payne.name>
2024-06-20 20:58:43 +1200

    Set up User State

--- app.js
+++ app.js
@@ -57,9 +57,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res, next) => {
+app.use((req,res, next) => {                    // Global stuff
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
+    res.locals.currentUser = req.user;
     next();
 });
 
