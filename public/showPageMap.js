7c92d67ab1193d106f1ca428e3ee50bce7d9f291
ZianPayne <zian@payne.name>
2024-06-28 21:44:08 +1200

    Finishing geocoded location

--- public/showPageMap.js
+++ public/showPageMap.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = '<%-process.env.MAPBOX_TOKEN%>';
+mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: "map",
     style: 'mapbox://styles/mapbox/streets-v11',
