4e703d1c401dca24ca97d46cdffdb75d030c34ce
ZianPayne <zian@payne.name>
2024-06-29 13:03:26 +1200

    Adding Colour Pins

--- public/javascript/clusterMap.js
+++ public/javascript/clusterMap.js
@@ -35,20 +35,20 @@ map.on('load', () => {
             'circle-color': [
                 'step',
                 ['get', 'point_count'],
-                '#51bbd6',
-                100,
-                '#f1f075',
-                750,
-                '#f28cb1'
+                '#38c960',
+                5,
+                '#20ba1a',
+                10,
+                '#1d8519'
             ],
             'circle-radius': [
                 'step',
                 ['get', 'point_count'],
-                20,
-                100,
-                30,
-                750,
-                40
+                10,
+                5,
+                15,
+                10,
+                20
             ]
         }
     });
