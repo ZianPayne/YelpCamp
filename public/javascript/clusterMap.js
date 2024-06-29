bb1f64201d6067ba0b4ab076007e17db85bc531c
ZianPayne <zian@payne.name>
2024-06-29 21:45:09 +1200

    Updating Map Styles with app.css stylesheet

--- public/javascript/clusterMap.js
+++ public/javascript/clusterMap.js
@@ -1,6 +1,6 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/dark-v11',
     center: [174.8860, -40.9006],
