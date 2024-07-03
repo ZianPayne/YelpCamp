2a1b0be7f319b2023e0abf8dcade23898b96bab6
ZianPayne <zian@payne.name>
2024-07-03 17:42:29 +1200

    Romeving Unsupported Mongo Options

--- app.js
+++ app.js
@@ -33,8 +33,6 @@ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology : true,
-    useCreateIndex : true,
-    useFindAndModify: false
 });
 
 const db = mongoose.connection; 
