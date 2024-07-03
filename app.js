09b5f5b83d91f5db1c0d0a8da9e3c5b02ec541b9
ZianPayne <zian@payne.name>
2024-07-03 17:52:14 +1200

    Fixing Mongo Settings

--- app.js
+++ app.js
@@ -31,8 +31,6 @@ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 
 mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology : true,
 });
 
 const db = mongoose.connection; 
