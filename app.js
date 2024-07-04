2ae9d4deb5fa3d8318d6fd4477720abe4b4fd9dd
ZianPayne <zian@payne.name>
2024-07-04 13:54:07 +1200

    Finishing Environment Management

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ const userRoutes = require('./routes/users.js');
 // Environment Variables
 const env = process.env.NODE_ENV || 'development';
 require('dotenv').config({ path: `.env.${env}` });
+console.log(`Environment: ${env}`);
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({path : "./.env"});
 
 const Campground = require('../models/campground');
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
