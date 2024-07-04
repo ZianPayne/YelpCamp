2aa0fc5f8500551c259a5db4abd7ba51a71122eb
ZianPayne <zian@payne.name>
2024-07-04 13:33:08 +1200

    Adding Scripts for Environment

--- app.js
+++ app.js
@@ -25,7 +25,10 @@ const campgroundRoutes = require('./routes/campgrounds.js');
 const reviewRoutes = require('./routes/reviews.js');
 const userRoutes = require('./routes/users.js');
 
-// Database URL
+// Environment Variables
+const env = process.env.NODE_ENV || 'development';
+require('dotenv').config({ path: `.env.${env}` });
+
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl).then(() => {
