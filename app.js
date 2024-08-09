56c1ffddd312bafa4a4cee8534835adda54ca049
Z'Arn <119647505+ZianPayne@users.noreply.github.com>
2024-08-09 22:22:25 +1200

    Working towards fixing Image Upload Issue #9

--- app.js
+++ app.js
@@ -66,8 +66,8 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
-app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.use(mongoSanitize({
     replaceWith: "_"
