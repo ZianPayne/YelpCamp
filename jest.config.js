ac0601919210ef439da220bb501bb844348c7f02
Z'Arn <119647505+ZianPayne@users.noreply.github.com>
2024-08-24 18:41:37 +1200

    Adding test structure, issue #11

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    testEnvironment: 'node',
+    roots: ['<rootDir>/tests'],
+    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+    moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
+};
